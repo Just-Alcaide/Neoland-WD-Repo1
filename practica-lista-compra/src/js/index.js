be079846f43fc185d209aef73b9360b83858247a
Alberto Alcaide <justalcaide@gmail.com>
2025-01-13 14:49:54 +0100

    style: :lipstick: Overhayl index, added shoppinglist + JS

---
+++ practica-lista-compra/src/js/index.js
@@ -0,0 +1,21 @@
+// 1. definimos (creamos) nuestra lista de la compra
+//var shoppingList = ['carne', 'pescado', 'fruta']
+
+//usamos var aquí, pero cuando trabajemos vamos a definir con let o con const
+let shoppingList = ['carne', 'pescado', 'fruta']
+let totalAmount = 0 //para variables que podemos modificar
+const PERAS = 'peras' //para variables que no pueden modificar, se ponen en mayúscula
+
+
+//para imprimir la lista de la compra. vamos a usar console.log
+console.log(shoppingList, totalAmount, PERAS)
+//podemos hacer print de pseudocódigo
+/*
+console.info(shoppingList)
+console.error(shoppingList)
+las listas, en lugar de empezar en 1 empieza en 0
+*/
+
+//no definido es no existe
+
+//vamos a trabajar cambiando contenido de shoppingList y recalculamos el totalAmount

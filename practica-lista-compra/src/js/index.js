7b1526e05684a5c55a14d6fcbff1f808dc41317a
Alberto Alcaide <justalcaide@gmail.com>
2025-01-13 17:39:10 +0100

    feat: :sparkles: add object shopping list

--- practica-lista-compra/src/js/index.js
+++ practica-lista-compra/src/js/index.js
@@ -1,8 +1,17 @@
 let shoppingList = []
 
 function addToShoppingList () {
-    let newArticle = document.getElementById('articulo').value
-    shoppingList.push(newArticle)
+    /*let newArticle = document.getElementById('articulo').value
+    shoppingList.push(newArticle)*/
+    let newArticleName = document.getElementById('articulo').value
+    let newArticleQty = document.getElementById('qty').value
+    let newArticlePrice = document.getElementById('price').value
+    let newarticleObject = {
+        qty: newArticleQty,
+        name: newArticleName,
+        price: newArticlePrice,
+    }
+    shoppingList.push(newarticleObject)
     console.log('addToShoppingList', shoppingList)
 }
 
@@ -14,3 +23,14 @@ function resetShoppingList (){
 function changeColor () {
     document.getElementsByTagName('body')[0].style.backgroundColor = 'silver'
 }
+
+//vamos a ver como añadirlo como objeto
+/* el la función pilla el value, guarda la cadena de texto
+queremos que añada el objeto, 
+*/
+
+/*let newarticleObject = {
+    qty: 0,
+    name: newArticle,
+    price: 0,
+}*/

d3d73f01fd314ee1ff51b7c65817ea42b573b71e
Alberto Alcaide <justalcaide@gmail.com>
2025-02-20 16:48:57 +0100

    docs: :fire: clubSearchItem no longer needed

--- Proyecto-final/src/js/components/bundle.js
+++ Proyecto-final/src/js/components/bundle.js
@@ -1,11 +1,9 @@
 import { LoginForm } from "./LoginForm/LoginForm.js";
 import { RegisterForm } from "./RegisterForm/RegisterForm.js";
 import {ClubListItem} from "./ClubListItem/ClubListItem.js";
-import { ClubSearchItem } from "./ClubSearchItem/ClubSearchItem.js";
 import { ClubDetail } from "./ClubDetail/ClubDetail.js";
 
 export {LoginForm}
 export {RegisterForm}
 export {ClubListItem}
-export {ClubSearchItem}
 export {ClubDetail}
